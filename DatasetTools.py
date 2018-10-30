b60a6956e404f34e0ffa653a89d8ce0947175c93
lcalisto <luis.calisto@hotmail.com>
2018-10-30 17:24:16 +0100

    Fixed merge

--- DatasetTools.py
+++ DatasetTools.py
@@ -13,8 +13,8 @@ class DatasetTools():
            ".rm", ".swf", ".vob", ".wmv" ".docx", ".pdf",".rar", ".zip", ".7z", ".exe", 
            ".tar.gz", ".tar", ".mp3", ".sh", ".c", ".cpp", ".h", 
        ".gif", ".py", ".pyc", ".jar", ".sql", ".bundle",
-       ".html", ".php", ".log", ".bak", ".deb", "py","pdf","ppt","pptx","doc","docx","jar","shx","dbf","ini","dll","xls","xlsx")
-    def isHiddenWindows(self,filepath):
+       ".html", ".php", ".log", ".bak", ".deb", "py","pdf","ppt","pptx","doc","docx","jar","shx","dbf","ini","dll")
+    def isHiddenWindows(filepath):
         """ This function returns True is a file is hidden in windows"""
         try:
             return bool(os.stat(filepath).st_file_attributes & stat.FILE_ATTRIBUTE_HIDDEN)
@@ -172,10 +172,7 @@ class DatasetTools():
                 for filename in files:
                     relDir = os.path.relpath(dir_, folder)
                     relFile = os.path.join(relDir, filename)
-                    #In linux
                     relFile=relFile.replace('./', '')
-                    #In windows
-                    relFile = relFile.replace('.\\', '')
                     fullFile= os.path.join(dir_, filename)
                     # Excape windows hidden files.
                     if os.name=='nt':
@@ -214,66 +211,10 @@ class DatasetTools():
                     isIntersecting=self.checkRaster(pt,os.path.join(folder, filename),filename)
                     if isIntersecting:
                         intersectingRasters.append(filename)
-<<<<<<< HEAD
                 # check if file is a vector
                 if vector:
                     # in case of vectors we don't process folders.
                     if os.path.isdir(os.path.join(folder, filename)):
-=======
-                    continue
-                except:
-                    continue
-        return intersectingRasters
-    
-    def getVectors(self, pt, folder, recursive):
-        '''
-        Process vector files and return intersecting vectos
-        '''
-        #loop in the folder files
-        #print('start the loop',folder)
-        intersectingVectors=[]
-        if recursive:
-            for dir_, _, files in os.walk(folder):
-                # exclude linux hidden files and folders
-                if os.name=='posix':
-                    files[:] = [f for f in files if not f.startswith('.')]
-                    _[:] = [f for f in _ if not f.startswith('.')]
-                # exclude the folowing extencions, both windows or linux
-                files[:] = [f for f in files if not f.endswith(self.exclude)]
-                for filename in files:
-                    relDir = os.path.relpath(dir_, folder)
-                    relFile = os.path.join(relDir, filename)
-                    #in linux
-                    relFile=relFile.replace('./', '')
-                    #in Windows
-                    relFile = relFile.replace('.\\', '')
-                    fullFile= os.path.join(dir_, filename)
-                    # Excape windows hidden files.
-                    if os.name=='nt':
-                        if self.isHiddenWindows(fullFile):
-                            continue
-                    #removing folders ('')
-#                     if filename.endswith(''):
-#                         continue
-                    try:
-                        #Open vector file and get its extent
-                        ds=ogr.Open(fullFile)
-                        #todo loop to all the vector layers and check if the point intersects at least one Layer. Shapes only have one layer but other formats may have more.
-                        layer=ds.GetLayer(0)
-                        layerExtents=layer.GetExtent()
-                        #print("x_min = %.2f x_max = %.2f y_min = %.2f y_max = %.2f" % (layerExtents[0], layerExtents[1], layerExtents[2], layerExtents[3]))
-                        # reproject the extent to 4326 because the point will always be 4326 (to keep srs consistency)
-                        src_srs=osr.SpatialReference()
-                        src_srs.ImportFromWkt(str(layer.GetSpatialRef()))
-                        tgt_srs=osr.SpatialReference()
-                        tgt_srs.ImportFromEPSG(4326)
-                        layerExt=[[layerExtents[0],layerExtents[2]],[layerExtents[1],layerExtents[3]]]
-                        wgs84_ext=self.ReprojectCoords(layerExt,src_srs,tgt_srs)
-                        #Check if point intersects this pair of coordinates (vector extents)
-                        isIntersecting=self.CheckIntersection(pt,wgs84_ext,True)
-                        if isIntersecting:
-                            intersectingVectors.append(relFile)
->>>>>>> branch 'master' of https://github.com/lcalisto/qgis-finddataset-plugin.git
                         continue
                     #Check if vector intersects. If we get None then it's not a geo file!
                     isIntersecting,layerNames=self.checkVector(pt,os.path.join(folder, filename),filename)
